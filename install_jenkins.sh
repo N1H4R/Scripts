9601dbba40d0b0b3db4b438af66655f0424bfde8
N1H4R <nityananda_pradhan@yahoo.in>
2024-11-11 13:10:27 +0530

    Create install_jenkins.sh
    Initial version of install jenkins script.

---
+++ install_jenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This task is part of Jenkins Jumpstart Lab
+
+sudo apt install java-11 -y
+#sudo apt install openjdk-17-jdk -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt install jenkins
+
+systemctl status jenkins
